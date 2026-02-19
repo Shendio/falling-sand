87d7cc849d596c4a02455db6b5d467466db9e60a
Shendio <41945267+Shendio@users.noreply.github.com>
2026-02-19 19:32:24 +0100

    refactor: move the app logic into seperate file

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,8 @@
-use std::sync::Arc;
+mod app;
 
-use pixels::{Pixels, SurfaceTexture};
-use winit::{
-    application::ApplicationHandler,
-    dpi::LogicalSize,
-    event::WindowEvent,
-    event_loop::{ControlFlow, EventLoop},
-    window::Window,
-};
+use winit::event_loop::{ControlFlow, EventLoop};
 
-const WIDTH: u32 = 400;
-const HEIGHT: u32 = 400;
-const SCALE: f32 = 2.0;
-
-const TITLE: &str = "Falling Sand Simulator";
-
-#[derive(Default)]
-struct FallingApp {
-    window: Option<Arc<Window>>,
-    pixels: Option<Pixels<'static>>,
-}
-
-impl ApplicationHandler for FallingApp {
-    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
-        let size = LogicalSize::new(WIDTH as f32 * SCALE, HEIGHT as f32 * SCALE);
-        let attribs = Window::default_attributes()
-            .with_title(TITLE)
-            .with_inner_size(size);
-        let window = Arc::new(
-            event_loop
-                .create_window(attribs)
-                .expect("ERROR: Failed to create a window."),
-        );
-        self.window = Some(window.clone());
-
-        let inner_size = window.inner_size();
-        let surface_texture = SurfaceTexture::new(inner_size.width, inner_size.height, window);
-        let pixels = Pixels::new(WIDTH as u32, HEIGHT as u32, surface_texture)
-            .expect("ERROR: Failed to create a pixel buffer.");
-
-        self.pixels = Some(pixels);
-    }
-    fn window_event(
-        &mut self,
-        event_loop: &winit::event_loop::ActiveEventLoop,
-        _window_id: winit::window::WindowId,
-        event: winit::event::WindowEvent,
-    ) {
-        match event {
-            WindowEvent::CloseRequested => {
-                event_loop.exit();
-            }
-            WindowEvent::RedrawRequested => {
-                if let (Some(window), Some(pixels)) = (self.window.as_ref(), self.pixels.as_mut()) {
-                    let frame = pixels.frame_mut();
-
-                    for pixel in frame.chunks_exact_mut(4) {
-                        pixel.copy_from_slice(&[0x15, 0x15, 0x15, 0xFF]);
-                    }
-
-                    if let Err(e) = pixels.render() {
-                        println!("ERROR: Failed to render pixels: {:?}", e);
-                        event_loop.exit();
-                    }
-                    window.request_redraw();
-                }
-            }
-            _ => (),
-        }
-    }
-}
+use crate::app::FallingApp;
 
 fn main() {
     let mut app = FallingApp::default();
